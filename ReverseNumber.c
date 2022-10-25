f8dcc8ebd16b0340cd5a6a923e6b716d9d6e0c3d
weldsh2535 <weldetsadik2535@gmail.com>
2022-10-25 12:23:06 +0300

    reverse the numbers

---
+++ ReverseNumber.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int nums,rev=0,remanider;
+    printf("Enter the number:\n");
+    scanf("%d",&nums);
+    while (nums>0)
+    {
+        remanider= nums%10;
+        rev = rev*10+remanider;
+         nums = nums/10;
+    }
+    printf("The Reverse %d is %d",nums,rev);
+    return 0;
+}
