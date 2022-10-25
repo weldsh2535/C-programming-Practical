a8a896e4e618778576de476f28796fa225c35a4c
weldsh2535 <weldetsadik2535@gmail.com>
2022-10-25 14:26:54 +0300

    power the number

---
+++ power.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int n,power;
+    printf("ENter the number:\n");
+    scanf("%d",&n);
+    power =n*n;
+    printf("The %d power of %d \n",n,power);
+    return 0;
+}
