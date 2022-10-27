1179689b2b55770100b36aba76ef5fd7505ef002
weldsh2535 <weldetsadik2535@gmail.com>
2022-10-27 14:18:57 +0300

    sample calculator for switch

---
+++ sampleCalculator.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main(){
+int n1,n2;
+double res;
+char op;
+printf("Enter the operator(+,-,*,/):\n");
+scanf("%c",&op);
+printf("Enter the number :\n");
+scanf("%d %d",&n1,&n2);
+
+switch(op){
+case '+':
+ res=n1+n2;
+ printf("Sum = %lf",res);
+ break;
+case '-':
+ res=n1-n2;
+ printf("Substraction = %lf",res);
+ break;
+ case '*':
+ res=n1*n2;
+ printf("Multiply = %lf",res);
+ break;
+ case '/':
+ res=n1/n2;
+ printf("Division = %lf",res);
+ break;
+}
+return 0;
+}
